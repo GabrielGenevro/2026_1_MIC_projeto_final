8e9c180e5c7c3cd29aaf2ea0f8381236421c37e8
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-06-12 15:02:58 -0300

    [feat] criação da solução e projeto exemplo

---
+++ equipes/exemplo/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 6/12/2026 3:00:37 PM
+ *  Author: Professor
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
