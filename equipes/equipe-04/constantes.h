639e4def61ae837cd3e3c81c1b0dbd79e750b14a
Gabriel Genevro <gabrielgenevro01@gmail.com>
2026-06-19 17:43:58 -0300

    criação de arquivo para constantes criação de arquivo  para configurações do adc Alteração do main para interagir com os arquivos

---
+++ equipes/equipe-04/constantes.h
@@ -0,0 +1,14 @@
+/*
+ * constantes.h
+ *
+ * Created: 19/06/2026 16:55:26
+ *  Author: Aluno
+ */ 
+
+
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+const vref = 5; //V de referencia da temperatura
+
+#endif /* CONSTANTES_H_ */
--- equipes/equipe-04/main.c
+++ equipes/equipe-04/main.c
@@ -2,15 +2,32 @@
  * main.c
  *
  * Created: 6/19/2026 4:29:16 PM
- *  Author: Aluno
+ *  Author: Gabriel Genevro
  */ 
+#define F_CPU 16000000
 
 #include <xc.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include "constantes.h"
+#include "adc.c"
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+
+ISR(INT0_vect){
+
 }
+
+void config(){
+	DDRB |= (1<<PORTB5);
+	float temperatura;
+}
+
+int main(void){
+	ADC_Init();
+	sei();
+    while(1){
+			uint8_t adc = ADC_Read(0);
+			temperatura = (adc * vref * 100.0)/1024;
+		}
+	}
